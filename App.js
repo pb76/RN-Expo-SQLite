fa1eaa68f4ec1881165783f814c8e8703665436b
Patrik Bernhardsson <pb76@users.noreply.github.com>
2024-10-24 13:34:15 +0200

    Lade till kopiering mellan databaser

--- App.js
+++ App.js
@@ -8,15 +8,16 @@ let db;
 
 export default function App() {
   const [SQLiteData, setSQLiteData] = useState([]);
+  const [SQLitePermData, setSQLitePermData] = useState([]);
   const [varde, setVarde] = useState("");
   const [intVarde, setIntVarde] = useState(0);
 
   useEffect(() => {
     openDB();
   }, []);
-  useEffect(() => {
+  /*useEffect(() => {
     readData();
-  }, [SQLiteData]);
+  }, [SQLiteData]);*/
 
   const readData = async () => {
     let tabelData = [];
@@ -36,12 +37,49 @@ export default function App() {
         "INSERT INTO test (value, intValue) VALUES ('" + varde + "', " + intVarde + ")"
       );
     }
-    console.log(result.lastInsertRowId, result.changes);
+    //console.log(result.lastInsertRowId, result.changes);
+    readData();
   };
 
   const deleteLastRow = async () => {
     const result = await db.execAsync("DELETE FROM test WHERE id = (SELECT MAX(id) FROM test)");
-    console.log(result.lastInsertRowId, result.changes);
+    console.log("deleteLastRow:", result);
+    //console.log("deleteLastRow:", result.lastInsertRowId, result.changes);
+    readData();
+  };
+
+  const deleteLastRowPerm = async () => {
+    const result = await db.execAsync("DELETE FROM test_perm WHERE id = (SELECT MAX(id) FROM test_perm)");
+    console.log("deleteLastRow:", result);
+    readPermData();
+  };
+
+  const moveData = async () => {
+    console.log("Move data from Temp to Perm");
+    const allRows = await db.getAllAsync("SELECT * FROM test");
+    for (const row of allRows) {
+      console.log(row.id, row.value, row.intValue);
+      console.log("row:", row);
+      const result = await db.execAsync(
+        "INSERT INTO test_perm (value, intValue) VALUES ('" + row.value + "', " + row.intValue + ")"
+      );
+      console.log(result);
+      const resultDelete = await db.execAsync("DELETE FROM test WHERE id = " + row.id);
+      console.log("delete", resultDelete);
+    }
+    readData();
+    readPermData();
+  };
+
+  const readPermData = async () => {
+    let tabelData = [];
+    const allRows = await db.getAllAsync("SELECT * FROM test_perm");
+    for (const row of allRows) {
+      console.log(row.id, row.value, row.intValue);
+      //console.log(row);
+      tabelData.push(row);
+    }
+    setSQLitePermData(tabelData);
   };
 
   return (
@@ -54,9 +92,10 @@ export default function App() {
         placeholder="Skriv siffra"
         onChangeText={(intVarde) => setIntVarde(intVarde)}
       />
-      <Button title="Write Data" onPress={writeData} />
-      <Button title="Read Data" onPress={readData} />
-      <Button title="Delete Last Row" onPress={deleteLastRow} />
+      <Button title="Write Data in Temp" onPress={writeData} />
+      <Button title="Read Data in Temp" onPress={readData} />
+      <Button title="Delete Last Row in Temp" onPress={deleteLastRow} />
+      <Text>Temp database:</Text>
       {SQLiteData.map((item, index) => {
         return (
           <View key={index}>
@@ -66,7 +105,20 @@ export default function App() {
           </View>
         );
       })}
-      <Text>{JSON.stringify(SQLiteData)}</Text>
+      {/* <Text>{JSON.stringify(SQLiteData)}</Text>  */}
+      <Button title="Move Data from Temp to Perm" onPress={moveData} />
+      <Button title="Read Data in Perm" onPress={readPermData} />
+      <Button title="Delete Last Row in Perm" onPress={deleteLastRowPerm} />
+      <Text>Perm database:</Text>
+      {SQLitePermData.map((item, index) => {
+        return (
+          <View key={index}>
+            <Text>
+              {item.id}, {item.value}, {item.intValue}
+            </Text>
+          </View>
+        );
+      })}
       <StatusBar style="auto" />
     </View>
   );
@@ -78,6 +130,10 @@ const openDB = async () => {
     PRAGMA journal_mode = WAL;
     CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);
     `);
+  await db.execAsync(`
+      PRAGMA journal_mode = WAL;
+      CREATE TABLE IF NOT EXISTS test_perm (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);
+      `);
 };
 
 const styles = StyleSheet.create({
