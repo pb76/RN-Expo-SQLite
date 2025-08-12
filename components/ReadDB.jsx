99f9df017ae5940157428bd805cc503cb460970a
Patrik Bernhardsson <patrik@bernhardsson.com>
2025-08-12 11:30:14 +0200

    Add ReadDB component to fetch and display users from the database

---
+++ components/ReadDB.jsx
@@ -0,0 +1,30 @@
+import { getDatabase } from "../utils/database";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+export default function ReadDB() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    readData();
+  }, []);
+
+  const readData = async () => {
+    const db = await getDatabase();
+    const allRows = await db.getAllAsync("SELECT * FROM users");
+    setUsers(allRows);
+  };
+
+  return (
+    <View>
+      <Text>Users List</Text>
+      <Button title="Refresh" onPress={readData} />
+      {users.length === 0 && <Text>No users found</Text>}
+      {users.map((user) => (
+        <Text key={user.id}>
+          {user.name} - {user.email}
+        </Text>
+      ))}
+    </View>
+  );
+}
