af3bce679f95f13d1a400d0221f158b3865c6d92
Patrik Bernhardsson <patrik@bernhardsson.com>
2025-08-12 11:09:46 +0200

    Refactor App component to initialize database on startup and improve formatting

--- App.jsx
+++ App.jsx
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { initDatabase } from "./utils/database";
+import { useEffect } from "react";
+import ReadDB from "./components/ReadDB";
 
 export default function App() {
+  useEffect(() => {
+    // Initialize the database when the app starts
+    initDatabase()
+      .then(() => {
+        console.log("Database initialized successfully");
+      })
+      .catch((error) => {
+        console.error("Database initialization failed:", error);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <ReadDB />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
